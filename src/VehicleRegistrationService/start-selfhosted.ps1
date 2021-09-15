496c4db0167aa543b0e102134e4931fe7aafed8e
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-09-15 20:33:37 +0200

    Replace startup script

---
+++ src/VehicleRegistrationService/start-selfhosted.ps1
@@ -0,0 +1,1 @@
+dapr run --app-id vehicleregistrationservice --app-port 6002 --dapr-http-port 3602 --dapr-grpc-port 60002 --config ../dapr/config/config.yaml --components-path ../dapr/components -- uvicorn vehicle_registration:app --port 6002
