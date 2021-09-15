a0a4f1e76475107817fb5a7e92cc1684244b8762
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-09-15 21:16:52 +0200

    Add state management

--- src/TrafficControlService/traffic_control/repositories.py
+++ src/TrafficControlService/traffic_control/repositories.py
@@ -1,15 +1,17 @@
 from . import models
+from dapr.clients import DaprClient
 
 
 class VehicleStateRepository:
     def __init__(self):
-        self.state = {}
+        pass
 
     def get_vehicle_state(self, license_number: str) -> models.VehicleState or None:
-        if license_number in self.state:
-            return self.state[license_number]
-        else:
-            return None
+        with DaprClient() as client:
+            return models.VehicleState.parse_raw(client.get_state("statestore", license_number).text())
+
 
     def set_vehicle_state(self, vehicle_state: models.VehicleState) -> None:
-        self.state[vehicle_state.license_number] = vehicle_state
+        with DaprClient() as client:
+            client.save_state("statestore", vehicle_state.license_number, vehicle_state.json())
+
