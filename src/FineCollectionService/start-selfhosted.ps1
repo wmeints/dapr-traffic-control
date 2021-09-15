ee673ec1c6d624453dd40ed3b175d23a44a80cbd
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-09-15 20:41:49 +0200

    Add startup script

---
+++ src/FineCollectionService/start-selfhosted.ps1
@@ -0,0 +1,1 @@
+dapr run --app-id finecollectionservice --app-port 6001 --dapr-http-port 3601 --dapr-grpc-port 60001 --config ../dapr/config/config.yaml --components-path ../dapr/components -- uvicorn fine_collection:app --port 6001
