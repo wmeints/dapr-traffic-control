0e0d0d3f1dfc279d0e7d11ce932b0bbe789fac00
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-09-15 21:38:12 +0200

    Add secret store

--- src/FineCollectionService/fine_collection/__init__.py
+++ src/FineCollectionService/fine_collection/__init__.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import Response
 from . import models, settings, services, clients
+from dapr.clients import DaprClient
 
 
 app = FastAPI()
 app_settings = settings.ApplicationSettings()
 
+with DaprClient() as dapr_client:
+    license_key = dapr_client.get_secret("trafficcontrol-secrets", "finecalculator.licensekey")
+
 processor = services.ViolationProcessor(
-    services.FineCalculator(), 
+    services.FineCalculator(license_key),
     clients.VehicleRegistrationClient()
 )
 
